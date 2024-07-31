4d8ed4a8f0768272ed500fc4121be468f79f5d90
marlabharghavsai <147792275+marlabharghavsai@users.noreply.github.com>
2024-07-31 15:44:55 +0530

    import java.util.*; public class name{     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int x,y,z;         x=read.nextInt();         y=read.nextInt();         z=read.nextInt();         System.out.println(((x*5)+(y*10))/z);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class name{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int x,y,z;
+        x=read.nextInt();
+        y=read.nextInt();
+        z=read.nextInt();
+        System.out.println(((x*5)+(y*10))/z);
+    }
+}
