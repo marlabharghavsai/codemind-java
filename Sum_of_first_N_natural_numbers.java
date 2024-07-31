13f3a74c9e09c25845abb66e7f6bb4f79c7bab6d
marlabharghavsai <147792275+marlabharghavsai@users.noreply.github.com>
2024-07-31 15:29:02 +0530

    import java.util.*; public class name{     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int n;         n=read.nextInt();         System.out.println((n*(n+1))/2);     } }

---
+++ Sum_of_first_N_natural_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class name{
+    public static void main(String[] args)
+    {
+        Scanner read=new Scanner(System.in);
+        int n;
+        n=read.nextInt();
+        System.out.println((n*(n+1))/2);
+    }
+}
